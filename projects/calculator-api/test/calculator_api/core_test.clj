1c439f9e621dba324704bbfdf75cceff7542f06c
Vili Heikkilä <t95371@gmail.com>
2020-05-17 13:20:45 +0300

    initial commit for calculator-api

---
+++ projects/calculator-api/test/calculator_api/core_test.clj
@@ -0,0 +1,7 @@
+(ns calculator-api.core-test
+  (:require [clojure.test :refer :all]
+            [calculator-api.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
