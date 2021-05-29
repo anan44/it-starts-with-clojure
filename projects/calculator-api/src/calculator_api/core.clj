9ad8498f8e88f776f768dc1e40d60a4598f19f96
Mauricio <mauricio@pop-os.localdomain>
2021-05-29 00:37:24 -0300

    Added an alternate way to start the server + explanation

--- projects/calculator-api/src/calculator_api/core.clj
+++ projects/calculator-api/src/calculator_api/core.clj
@@ -1,7 +1,9 @@
 (ns calculator-api.core
   (:gen-class))
 
+(require '[calculator-api.server.server :as calculator-server])
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (calculator-server/start))
